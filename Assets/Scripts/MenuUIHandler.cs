13b9532ad84568e0f214a199fab9c3a1fa3130cb
jdog207 <jdoganimallover@gmail.com>
2025-01-28 10:00:37 -0600

    allmost....

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEditor;
+using TMPro;
+
 public class MenuUIHandler : MonoBehaviour
 {
     //This is the handler of the main menu scene
